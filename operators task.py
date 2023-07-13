da5f20be85b3b8f3de6d0a90cbf202d653da1561
rafah <rafah.hawa@yahoo.fr>
2023-07-13 19:45:47 +0200

    add operators task

---
+++ operators task.py
@@ -0,0 +1,42 @@
+'''Task operators'''
+'''1'''
+'''(=)is used to assigne a value to a variable. (==)is used in comparison'''
+
+'''2'''
+'''=! check if the two values are not equal'''
+
+'''3'''
+'''Operator Precedence determinate the order of operations '''
+
+'''4'''
+x=10
+
+'''5'''
+x+=15
+print(x)
+
+'''6'''
+x/=5
+print(x)
+
+'''7'''
+x*=10
+print(x)
+
+'''8'''
+x%=3
+print(x)
+
+'''9'''
+print(11%4)
+
+'''10'''
+print(2**3)
+
+'''11'''
+print(11/3)
+
+'''12'''
+print(int(11/3))
+
+
