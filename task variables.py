0fdf95d919f4fdfa914b3990a7a699a71370fce5
rafah <rafah.hawa@yahoo.fr>
2023-07-13 19:22:33 +0200

    add variables task

---
+++ task variables.py
@@ -0,0 +1,59 @@
+#Task Variables
+'''1'''
+
+x=bool()
+x1=True
+print(type(x))
+print(type(x1))
+
+'''2'''
+y=int()
+y1=5
+print(type(y))
+print(type(y1))
+
+'''3'''
+z=float()
+z1=6.8
+print(type(z))
+print(type(z1))
+
+'''4'''
+w=str()
+w1='mohamad'
+print(type(w))
+print(type(w1))
+
+
+'''5'''
+y1=float(y1)
+print(type(y1))
+
+'''6'''
+'''we can not turn str to int'''
+
+'''7'''
+l=[1,2,3,4,5]
+print (type(l))
+
+'''8'''
+t=(10,11,12,13,14,15)
+print(type(t))
+
+'''9'''
+l=tuple(l)
+print(type(l))
+
+'''10'''
+d={1:'ali',2:'yasmeen',3:'jad'}
+print(len(d))
+
+'''11'''
+'''yes if we want to place more than one statement on a single line'''
+
+'''12'''
+'''python is an interpreted language'''
+
+'''13'''
+'''high level languages can be read easily by humans
+low level languages or binary can be read easily by computers'''
